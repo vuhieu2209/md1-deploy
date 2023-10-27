e69762b7755e651ca9aba32c060ea2197a010383
unknown <vuminhhieu22092001@gmail.com>
2023-10-27 23:56:21 +0700

    Deploy

---
+++ User.js
@@ -0,0 +1,18 @@
+class User {
+    id;
+    name;
+    username;
+    email;
+    password;
+    avatar;
+
+    constructor(id, name, username, email, password, avatar) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.avatar = avatar;
+    }
+
+}
